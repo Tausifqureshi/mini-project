ea4fde86872592a19c5ad88183060e4cbd86da82
Tausifqureshi <tausifqureshi504@gmail.com>
2024-08-11 09:16:14 +0530

    Products-Timers

--- Products-Timers/time.js
+++ Products-Timers/time.js
@@ -1,4 +1,26 @@
-var countDownDate = new Date("Aug 1, 2024 12:00:00").getTime();
+// var countDownDate = new Date("Aug 1, 2024 12:00:00").getTime();
+
+// var countdownFunction = setInterval(function () {
+//   var now = new Date().getTime();
+//   var distance = countDownDate - now;
+
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   document.getElementById("days").innerText = days;
+//   document.getElementById("hours").innerText = hours;
+//   document.getElementById("minutes").innerText = minutes;
+//   document.getElementById("seconds").innerText = seconds;
+
+//   if (distance < 0) {
+//     clearInterval(countdownFunction);
+//     document.getElementById("timer").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
+
+var countDownDate = new Date("Aug 20, 2024 12:00:00").getTime();
 
 var countdownFunction = setInterval(function () {
   var now = new Date().getTime();
